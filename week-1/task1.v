8abb28b7e360a23534b97d1946e935e29cc08ca9
chakri96032 <chakri96032@gmail.com>
2026-08-11 12:34:13 +0530

    Add testbench for ripple adder module

--- week-1/task1.v
+++ week-1/task1.v
@@ -10,3 +10,20 @@ module ripple_adder(A,B,Cin,Sum,Cout);
   full_adder fa2(A[2],B[2],c2,Sum[2],c3);
   full_adder fa3(A[3],B[3],c3,Sum[3],Cout);
 endmodule
+
+module tb_ripple_adder;
+  reg [3:0] A,B;
+  reg Cin;
+  wire [3:0] Sum;
+  wire Cout;
+
+  ripple_adder uut(A,B,Cin,Sum,Cout);
+
+  initial begin
+    $monitor("A=%b B=%b Cin=%b | Sum=%b Cout=%b",A,B,Cin,Sum,Cout);
+    A=4'b0000; B=4'b0000; Cin=0;
+    #10 A=4'b0011; B=4'b0101; Cin=0;
+    #10 A=4'b1111; B=4'b0001; Cin=1;
+    #10 $finish;
+  end
+endmodule
