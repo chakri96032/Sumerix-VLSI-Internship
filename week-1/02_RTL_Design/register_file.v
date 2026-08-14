7114818e533558b5aa5b2560d013150713397d2d
chakri96032 <chakri96032@gmail.com>
2026-08-14 13:01:27 +0530

    Implement register file module with read/write functionality

--- week-1/02_RTL_Design/register_file.v
+++ week-1/02_RTL_Design/register_file.v
@@ -1,1 +1,18 @@
+module register_file (
+    input clk,
+    input reg_write,
+    input [4:0] read_reg1, read_reg2, write_reg,
+    input [31:0] write_data,
+    output [31:0] read_data1, read_data2
+);
+    reg [31:0] registers [31:0];
+
+    assign read_data1 = registers[read_reg1];
+    assign read_data2 = registers[read_reg2];
+
+    always @(posedge clk) begin
+        if (reg_write && write_reg != 0)
+            registers[write_reg] <= write_data;
+    end
+endmodule
 
