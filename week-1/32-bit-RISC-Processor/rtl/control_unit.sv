0372df3e836a9a9cc4797624c26aae6458ab6589
chakri96032 <chakri96032@gmail.com>
2026-08-17 14:06:30 +0530

    Update control_unit.sv

--- week-1/32-bit-RISC-Processor/rtl/control_unit.sv
+++ week-1/32-bit-RISC-Processor/rtl/control_unit.sv
@@ -10,58 +10,57 @@ module control_unit (
     output logic       branch,
     output logic       jump,
 
-    output logic [2:0] alu_control
+    output logic [2:0] alu_op
 );
 
     always_comb begin
 
-        // Default values
-        reg_dst    = 1'b0;
-        alu_src    = 1'b0;
-        mem_to_reg = 1'b0;
-        reg_write  = 1'b0;
-        mem_read   = 1'b0;
-        mem_write  = 1'b0;
-        branch     = 1'b0;
-        jump       = 1'b0;
-        alu_control = 3'b000;
+        reg_dst     = 1'b0;
+        alu_src     = 1'b0;
+        mem_to_reg  = 1'b0;
+        reg_write   = 1'b0;
+        mem_read    = 1'b0;
+        mem_write   = 1'b0;
+        branch      = 1'b0;
+        jump        = 1'b0;
+        alu_op      = 3'b000;
 
         case (opcode)
 
             // R-type
             6'b000000: begin
-                reg_dst     = 1'b1;
-                reg_write   = 1'b1;
-                alu_control = 3'b000;
+                reg_dst   = 1'b1;
+                reg_write = 1'b1;
+                alu_op    = 3'b010;
             end
 
             // ADDI
             6'b001000: begin
-                alu_src     = 1'b1;
-                reg_write   = 1'b1;
-                alu_control = 3'b000;
+                alu_src   = 1'b1;
+                reg_write = 1'b1;
+                alu_op     = 3'b000;
             end
 
             // LW
             6'b100011: begin
-                alu_src     = 1'b1;
-                mem_to_reg  = 1'b1;
-                reg_write   = 1'b1;
-                mem_read    = 1'b1;
-                alu_control = 3'b000;
+                alu_src    = 1'b1;
+                mem_to_reg = 1'b1;
+                reg_write  = 1'b1;
+                mem_read   = 1'b1;
+                alu_op     = 3'b000;
             end
 
             // SW
             6'b101011: begin
-                alu_src     = 1'b1;
-                mem_write   = 1'b1;
-                alu_control = 3'b000;
+                alu_src   = 1'b1;
+                mem_write = 1'b1;
+                alu_op    = 3'b000;
             end
 
             // BEQ
             6'b000100: begin
-                branch      = 1'b1;
-                alu_control = 3'b001;
+                branch = 1'b1;
+                alu_op = 3'b001;
             end
 
             // JUMP
@@ -70,10 +69,11 @@ module control_unit (
             end
 
             default: begin
-                // Keep default control signals
+                // NOP
             end
 
         endcase
+
     end
 
 endmodule
